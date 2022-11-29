93aa0f7b31c2689e5e6f0d2031be2a39b736b864
Luka Varga <luka.varga6@skole.hr>
2022-11-29 09:37:34 +0100

    Initial commit

---
+++ app/src/main/java/com/example/varga_bmi/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.varga_bmi;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText height;
+    private EditText weight;
+    private TextView result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        height = (EditText) findViewById(R.id.visina);
+        weight = (EditText) findViewById(R.id.tezina);
+        result = (TextView) findViewById(R.id.calc);
+    }
+    public void calculateBMI(View v) {
+        String heightStr = height.getText().toString();
+        String weightStr = weight.getText().toString();
+
+        if (heightStr != null && !"".equals(heightStr)
+                && weightStr != null  &&  !"".equals(weightStr)) {
+            float heightValue = Float.parseFloat(heightStr) / 100;
+            float weightValue = Float.parseFloat(weightStr);
+
+            float bmi = weightValue / (heightValue * heightValue);
+
+            displayBMI(bmi);
+        }
+    }
+
+    private void displayBMI(float bmi) {
+        String bmiLabel = "";
+
+        if (Float.compare(bmi, 20f) <= 0) {
+            bmiLabel = getString(R.string.premalatezina);
+        } else if (Float.compare(bmi, 20f) > 0  &&  Float.compare(bmi, 25f) <= 0) {
+            bmiLabel = getString(R.string.normalnatezina);
+        } else if (Float.compare(bmi, 25f) > 0){
+            bmiLabel = getString(R.string.pretjeranatezina);
+        }
+
+        bmiLabel = bmi + "\n\n" + bmiLabel;
+        result.setText(bmiLabel);
+
+    }
+}
